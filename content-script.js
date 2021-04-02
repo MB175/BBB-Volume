3c1f03e8dd86551ad9627f9b5e7a62e7944f9dce
Mb175 <57674787+MB175@users.noreply.github.com>
2021-04-02 05:16:24 +0200

    feat: get Name of UserSlider

--- content-script.js
+++ content-script.js
@@ -1,7 +1,12 @@
+var dorpodwnPostfix = "]/div/div[2]/div/ul";
+var namePostfix = "]/div/div[1]/div/div[2]/span/span"
+var prefix =
+  "/html/body/div/main/section/div[2]/div/div/div/div[3]/div[2]/div/div/div[";
+
 console.log("BBB identified!");
 
 var container;
-var sliderList =  [];
+var sliderList = [];
 
 var observer = new MutationObserver(function (mutations) {
   if (
@@ -54,28 +59,27 @@ function checkIfSliderIsPresent(node) {
   return presnet;
 }
 
-
-function addSlider(slider) {
+function addSlider(slider, name) {
   slider.addEventListener("input", function () {
-    console.log("Set UserVolume to: " + slider.value);
+    console.log("Set " + name + " volume to: " + slider.value);
   });
 
-  sliderList.push(slider)
+  sliderList.push(slider);
 }
 
-
 function injectVolumeSlidersDropDown(target) {
-  var dorpodwnPostfix = "]/div/div[2]/div/ul";
-  var dropDownPrefix =
-    "/html/body/div/main/section/div[2]/div/div/div/div[3]/div[2]/div/div/div[";
 
   for (i = 1; i <= target.childNodes.length; i++) {
-    let query = dropDownPrefix + i + dorpodwnPostfix;
+    let query = prefix + i + dorpodwnPostfix;
     let el = getElementByXpath(query);
 
     if (checkIfSliderIsPresent(el)) {
       continue;
     }
+
+    let nameQuery = prefix + i + namePostfix;
+    let nameSpan = getElementByXpath(nameQuery)
+    let name = nameSpan.innerHTML.replace(/\&nbsp;/g, '')
 
     var slider = document.createElement("INPUT");
     slider.min = 0;
@@ -83,8 +87,7 @@ function injectVolumeSlidersDropDown(target) {
     slider.value = 100;
     slider.type = "range";
 
-    addSlider(slider)
-
+    addSlider(slider, name);
 
     var liElement = document.createElement("LI");
     liElement.className = "userVolumeSlider";
