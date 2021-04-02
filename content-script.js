6d4218e74dccac3eba73dd7e3d751c9797da90de
Mb175 <57674787+MB175@users.noreply.github.com>
2021-04-02 03:49:20 +0200

    feat: add slider per user in context

--- content-script.js
+++ content-script.js
@@ -1,5 +1,4 @@
-
-console.log("BBB identified")
+console.log("BBB identified!");
 
 var observer = new MutationObserver(function (mutations) {
   if (
@@ -11,7 +10,18 @@ var observer = new MutationObserver(function (mutations) {
       "/html/body/div/main/section/div[1]/section[2]/div/div[2]"
     );
 
+    var dropDown = getElementByXpath(
+      "/html/body/div/main/section/div[2]/div/div/div/div[3]/div[2]/div/div/div[3]/div/div[2]/div/ul"
+    );
+    var container = getElementByXpath(
+      "/html/body/div/main/section/div[2]/div/div/div/div[3]/div[2]/div/div"
+    );
+
+    console.log(container.childNodes.length);
+
     injectVolumeSlider(menuBarDiv);
+    injectVolumeSlidersDropDown(container);
+
     observer.disconnect();
   }
 });
@@ -21,6 +31,28 @@ observer.observe(document.body, {
   subtree: true,
 });
 
+function injectVolumeSlidersDropDown(target) {
+  var dorpodwnPostfix = "]/div/div[2]/div/ul";
+  var dropDownPrefix =
+    "/html/body/div/main/section/div[2]/div/div/div/div[3]/div[2]/div/div/div[";
+
+  for (i = 1; i <= target.childNodes.length; i++) {
+    var slider = document.createElement("INPUT");
+    slider.min = 0;
+    slider.max = 100;
+    slider.value = 100;
+    slider.type = "range";
+
+    var liElement = document.createElement("LI");
+    liElement.appendChild(slider);
+    let query = dropDownPrefix + i + dorpodwnPostfix;
+
+    let el = getElementByXpath(query);
+    console.log(el);
+    el.appendChild(liElement);
+  }
+}
+
 function injectVolumeSlider(target) {
   var slider = document.createElement("INPUT");
   slider.min = 0;
@@ -28,7 +60,6 @@ function injectVolumeSlider(target) {
   slider.value = 100;
   slider.type = "range";
   target.appendChild(slider);
-
 
   slider.addEventListener("input", function () {
     document.querySelector("audio").volume = slider.value / 100;
